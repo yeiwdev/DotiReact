350c8efeae8418bbf9509b4931539396deb95db1
yeiwdev <yeiwdev@gmail.com>
2024-10-10 18:58:23 +0900

    ch02/ch02_1

---
+++ ch02/ch02_1/src/index.tsx
@@ -0,0 +1,33 @@
+// import React from 'react'
+
+// let pPhysicalDOM = document.createElement(`p`)
+// pPhysicalDOM.innerText = `Hello physical DOM World!`
+// document.body.appendChild(pPhysicalDOM)
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.querySelector(`#root`) as HTMLElement)
+const pVirtuallDOM = React.createElement(`p`, null, `Hello World!`)
+
+root.render(pVirtuallDOM)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
