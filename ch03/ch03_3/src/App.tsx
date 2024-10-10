b7b38bebf8baafbbe1fdbf30ff7ace0a560cea0b
yeiwdev <yeiwdev@gmail.com>
2024-10-10 19:01:01 +0900

    ch03/ch03_3

---
+++ ch03/ch03_3/src/App.tsx
@@ -0,0 +1,31 @@
+import AvatarTest from "./pages/AvatarTest";
+import BackgroundImageTest from "./pages/BackgroundImageTest";
+import DisplayNoneTest from "./pages/DisplayNoneTest";
+import DisplayTest from "./pages/DisplayTest";
+import DivTest from "./pages/DivTest";
+import HeightTest from "./pages/HeightTest";
+import ImageTest from "./pages/ImageTest";
+import MarginTest from "./pages/MarginTest";
+import OverlayTest from "./pages/OverlayTest";
+import PaddingTest from "./pages/PaddingTest";
+import PositionTest from "./pages/PositionTest";
+import ViewportTest from "./pages/ViewportTest";
+
+export default function App() {
+  return (
+    <main>
+      <OverlayTest />
+      <PositionTest />
+      <DisplayNoneTest />
+      <DisplayTest />
+      <AvatarTest />
+      <BackgroundImageTest />
+      <ImageTest />
+      <MarginTest />
+      <PaddingTest />
+      <HeightTest />
+      <ViewportTest />
+      <DivTest />
+    </main>
+  );
+}
---
+++ ch03/ch03_3/src/components/Div.tsx
@@ -0,0 +1,43 @@
+import {
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes,
+  PropsWithChildren,
+} from "react";
+import { WidthHeight } from "./WidthHeight";
+import { LeftRightTopBottom } from "./LeftRightTopBottom";
+
+export type ReactDivProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+export type DivProps = ReactDivProps &
+  PropsWithChildren<WidthHeight> &
+  LeftRightTopBottom & { src?: string };
+
+export const Div: FC<DivProps> = ({
+  width,
+  height,
+  style: _style,
+  src,
+  className: _className,
+  left,
+  right,
+  top,
+  bottom,
+  ...props
+}) => {
+  const style = {
+    ..._style,
+    width,
+    height,
+    backgroundImage: src && `url(${src})`,
+    left,
+    right,
+    top,
+    bottom,
+  };
+
+  const className = [`box-border`, src && `bg-gray-300`, _className].join(` `);
+  return <div {...props} style={style} className={className} />;
+};
