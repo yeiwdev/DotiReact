75e69ea52c977c2bf6e8b3522bfa4ffc99162ef4
yeiwdev <yeiwdev@gmail.com>
2024-10-10 19:00:22 +0900

    ch03/ch03_1

---
+++ ch03/ch03_1/src/App.tsx
@@ -0,0 +1,17 @@
+import Bootstrap from './pages/Bootstrap'
+import Icon from './pages/Icon'
+import Style from './pages/Style'
+import UsingIcon from './pages/UsingIcon'
+import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass'
+
+export default function App() {
+  return (
+    <div>
+      <UsingIconWithCSSClass />
+      <UsingIcon />
+      <Style />
+      <Icon />
+      <Bootstrap />
+    </div>
+  )
+}
